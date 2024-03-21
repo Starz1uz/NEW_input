58f528e09f47f9dd29cb7bc8e179a63b9d60f8aa
Bexruz <148357539+Starz1uz@users.noreply.github.com>
2024-03-21 13:37:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const form = document.forms.namedItem('login')
+let inp = document.querySelector('.inp')
+let select = document.querySelector('select')
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+    let fm = new FormData(form)
+    let error = false
+    let option = false
+    let user = {
+        //name: fm.get('name'),
+        //age: fm.get('age')
+    }
+
+    fm.forEach((val, key) => {
+        user[key] = val
+
+        if (val.length === 0) {
+            error = true
+        }
+
+        if (select.value === "none") {
+            option = true;
+        }
+
+    })
+
+
+    if (error === true) {
+        alert('Fill all files')
+        return
+    }
+
+    if (option === true) {
+        alert('choose the lesson')
+        return
+    }
+
+    console.log(user);
+}
